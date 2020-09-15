a2ce94d1ff9ef5a327f72dbdbc56185a3ae195fb
Jeff Sutch <jeff.sutch@acm.org>
2020-09-15 14:03:47 -0700

    broke out routes into resources, added blueprints

--- movie-bag/app.py
+++ movie-bag/app.py
@@ -1,43 +1,14 @@
-from flask import Flask, jsonify, request, Response
 from database.db import initialize_db
-from database.models import Movie
+from resources.movie import movies
+
 
 app = Flask(__name__)
 
-movies = []
-
 app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb://localhost/movie-bag'
+ 'host': 'mongodb://localhost/movie-bag'
 }
 
 initialize_db(app)
-
-@app.route('/movies', methods=['GET'])
-def get_movies():
-    movies = Movie.objects().to_json()
-    return Response(movies, mimetype="application/json", status=200)
-
-@app.route('/movies/<id>')
-def get_movie(id):
-    movies = Movie.objects.get(id=id).to_json()
-    return Response(movies, mimetype="application/json", status=200)
-
-@app.route('/movies', methods=['POST'])
-def add_movie():
-    body = request.get_json()
-    movie = Movie(**body).save()
-    id = movie.id
-    return {'id': str(id)}, 200
-
-@app.route('/movies/<id>', methods=['PUT'])
-def update_movie(id):
-    body = request.get_json()
-    Movie.objects.get(id=id).update(**body)
-    return '', 200
-
-@app.route('/movies/<id>', methods=['DELETE'])
-def delete_movie(id):
-    Movie.objects.get(id=id).delete()
-    return '', 200
+app.register_blueprint(movies)
 
 app.run(debug=True)
--- movie-bag/resources/movie.py
+++ movie-bag/resources/movie.py
@@ -1,27 +1,33 @@
-@app.route('/movies')
+from flask import Blueprint, Response, request
+from database.models import Movie
+
+movies = Blueprint('movies', __name__)
+
+
+@movies.route('/movies')
 def get_movies():
  movies = Movie.objects().to_json()
- return Response(movies, mimetype="application/json", status=200)
+ return Response(movies, mimetype="movieslication/json", status=200)
 
-@app.route('/movies', methods=['POST'])
+@movies.route('/movies', methods=['POST'])
 def add_movie():
  body = request.get_json()
  movie = Movie(**body).save()
  id = movie.id
  return {'id': str(id)}, 200
 
-@app.route('/movies/<id>', methods=['PUT'])
+@movies.route('/movies/<id>', methods=['PUT'])
 def update_movie(id):
  body = request.get_json()
  Movie.objects.get(id=id).update(**body)
  return '', 200
 
-@app.route('/movies/<id>', methods=['DELETE'])
+@movies.route('/movies/<id>', methods=['DELETE'])
 def delete_movie(id):
  movie = Movie.objects.get(id=id).delete()
  return '', 200
 
-@app.route('/movies/<id>')
+@movies.route('/movies/<id>')
 def get_movie(id):
  movies = Movie.objects.get(id=id).to_json()
- return Response(movies, mimetype="application/json", status=200)
+ return Response(movies, mimetype="movieslication/json", status=200)
