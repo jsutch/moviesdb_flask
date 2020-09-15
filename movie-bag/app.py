01de636f909045de611a4bfd960c07e517cc55ab
Jeff Sutch <jeff.sutch@acm.org>
2020-09-15 11:24:42 -0700

    initial commit

---
+++ movie-bag/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+movies = [
+    {
+        "name": "The Shawshank Redemption",
+        "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
+        "genres": ["Drama"]
+    },
+    {
+       "name": "The Godfather ",
+       "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
+       "genres": ["Crime", "Drama"]
+    }
+]
+
+@app.route('/movies')
+def hello():
+    return jsonify(movies)
+
+
+app.run()
