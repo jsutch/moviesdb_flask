6daa857317b493f24a1112d6220cf4330fc32393
Jeff Sutch <jeff.sutch@acm.org>
2020-09-16 12:16:27 -0700

    authentication/authorization/jwt working

--- movie-bag/app.py
+++ movie-bag/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
+from flask_jwt_extended import JWTManager
+#
 import creds
 
 app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = creds.JWT_SECRET_KEY
+
 api = Api(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 
 app.config["MONGODB_SETTINGS"] = {
--- movie-bag/resources/movie.py
+++ movie-bag/resources/movie.py
@@ -1,4 +1,5 @@
 from flask import Response, request
+from flask_jwt_extended import jwt_required
 from database.models import Movie
 from flask_restful import Resource
 
@@ -7,6 +8,7 @@ class MoviesApi(Resource):
     movies = Movie.objects().to_json()
     return Response(movies, mimetype="application/json", status=200)
 
+  @jwt_required
   def post(self):
     body = request.get_json()
     movie = Movie(**body).save()
@@ -14,11 +16,12 @@ class MoviesApi(Resource):
     return {'id': str(id)}, 200
 
 class MovieApi(Resource):
+  @jwt_required
   def put(self, id):
     body = request.get_json()
     Movie.objects.get(id=id).update(**body)
     return '', 200
-
+  @jwt_required
   def delete(self, id):
     movie = Movie.objects.get(id=id).delete()
     return '', 200
